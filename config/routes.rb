800aacdc8796d1f2d6a1e3af4673981de47d38e6
nguyenloi <phucloi.2311@gmail.com>
2023-04-18 10:58:13 +0700

    create_users_table

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get "users/index"
+      # http://127.0.0.1:3000/api/v1/users/index
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
