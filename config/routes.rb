a40b92294f79cd4ae9d5592aa63711fa14fcf17e
nguyenloi <phucloi.2311@gmail.com>
2023-04-19 08:52:32 +0700

    gem versionist ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   api_version(:module => "V2", :path => { :value => "v2" }) do
-      resources :users
-    end
+    resources :users
+  end
   # api_version(:module => "V1", :path => { :value => "v1" }, :default => true) do
   api_version(:module => "V1", :path => { :value => "v1" }) do
     resources :users
   end
+
   # namespace :api do
   #   namespace :v1 do
   #     resources :users
